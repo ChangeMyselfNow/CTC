ccb66f24185fdc1b0533312280a5471f4118c94a
w.wang <w.wang@yoozoo.com>
2020-11-02 11:43:59 +0800

    优化

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -20,7 +20,7 @@
     <div class="footer_bottom">
       <div class="bottom_content">
         <p>Copyright © 2020-2020 CTC 版权所有 技术支持：CTC检查</p>
-        <p><a class="link_qzjd" href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2020101621号-1</a></p>
+        <p><a class="link_qzjd" href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2020101621号</a></p>
       </div>
     </div>
   </div>
